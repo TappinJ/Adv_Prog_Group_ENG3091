d42b5fd2575c848d2df7e6b6a955d92cea8e75aa
Alicja Potoniec <alicjahasinternet@outlook.com>
2026-03-29 02:41:03 +0100

    small fix, changed "m" from int to size_t. Thoroughly commented on Tool 2 - MLR

--- Assignment_1/Task_2/gradient_combined.h
+++ Assignment_1/Task_2/gradient_combined.h
@@ -108,16 +108,24 @@ void solve(){
 //=======================================
 
 struct MLR_gradient{
+    // Data storage//
     std::vector<double> x1;
     std::vector<double> x2;
     std::vector<double> y;
 
-    double w1 = 0.0;
-    double w2 = 0.0;
-    double b = 0.0;
-    const double n = 0.001;
+    // Initialised using uniform brace initialisation
+    // Prevents accidental narrowing conversions
+    // Initialised outside of the void function in case the variables are needed in the future
+    // Being outside of the void function makes these stand out.
+    double w1{};
+    double w2{};
+    double b{};
+    // n is the learning rate, controls the step size during the gradient descent. 
+        const double n{0.001};          // Value of 0.001 chosen to ensure stable convergence
+        const int epochs{1000};         // epochs - training iterations. Defines the number of passes over the dataset
 
     // 1 == DATA LOADER
+    // Void function as the dataset will no longer be needed after the parameters are saved in the data storage vectors
     void dataset(){
         std::ifstream file("multi_dataset.csv");
 
@@ -139,41 +147,81 @@ struct MLR_gradient{
 
 // 2 == Derivative Function Loop
 // Calculating all 3 gradients in a single pass
+// It calculates the steepness and error surface for all 3 parameters simultaneously.
+// The use of pass-by-reference is chosen to increase efficiency.
 void compute_gradients(double w1, double w2, double b, 
                         double& step_w1, double& step_w2, double& step_b)
 {
+
+    // ---Resetting the accumulators---
+    // Because step_w1, step_w2 and step_b are references 
+    // Setting them to 0.0 directly clears the values in the solve() function's memory
+    // Ensuresnew gradients aren't accidentally added to the previous iteration.
     step_w1 = 0.0;
     step_w2 = 0.0;
     step_b = 0.0;
-    int m = x1.size();
+    
+    // size_t prevents signed/unsigned comparison warnings
+    size_t m = x1.size();       // Scaling factor (m). 
+                                // Represents the total number of data points
+                                // Used to calculate the mean in mean squared error.
 
+    // ---Optimisation---
+    // Loop for optimising the batch gradient descent
+    // Iterates through the entire set (m samples) to calculate the collective error
     for(int i=0; i<m; i++){
-        double error = (w1 * x1[i]) + (w2 * x2[i]) + b - y[i];
-        step_w1 += error*x1[i];
-        step_w2 += error*x2[i];
-        step_b += error;
-    }
 
+        // 1 - Calculating the residual error (distance the model is from reality)
+        // compute prediction using the MLR state y = w1*x1 + w2*x2 + b
+        // error is the difference between this prediction and y[i]
+        double error = (w1 * x1[i]) + (w2 * x2[i]) + b - y[i];
+
+        // 2 - Accumulating the partial derivatives
+        // Chain rule - how much w1 contributed to the error
+        // multiply residual by the input value 
+        // these "step_" variables act as accumulators for the total gradient vector
+        step_w1 += error*x1[i];         // Derivative w.r.t. w1
+        step_w2 += error*x2[i];         // Derivative w.r.t. w2
+        step_b += error;                // Derivative w.r.t. bias
+    }
+    // 3 - Normalisation (the mean gradient)
+    // Divide by "m" to calculate the AVG gradient across the dataset
+    // Mean Squared Error approach - ensured total step size remains constant
+    // Regardless of the number of rows
+    // Kept outside of the loop for single pass optimisation
+    // saves (m-1) division operations
     step_w1 /= m;
     step_w2 /= m;
     step_b /= m;
 }
 
 // 3 == MAIN SOLVER LOOP ==
+// Iterative optimisation phase
+// Model "learns" by descending the surface towards the global minimum
+// inside "void" as values do not need returning - data is updated into vectors.
 void solve(){
     
-    for(int i=0; i<10000; i++){
-        double step_w1{};
-        double step_w2{};
-        double step_b{};
+    // loop runs through the fixed number of "epochs"
+    // Each epoch represents one full pass and update on the entire dataset
+    for(int i=0; i<epochs; i++){
 
+        double step_w1{};       // Accumulators for the current epoch's gradient vector
+        double step_w2{};       // Initialised to 0
+        double step_b{};        // New calculation for each iteration
+
+        // Calls the previously defined function
+        // Calculates the direction and magnitude of the steepest ascent
         compute_gradients(w1, w2, b, step_w1, step_w2, step_b);
 
+        // Update - parameter adjustment
+        // Gradient scaled by the learning rate
+        // Substracted to move the parameters in the opposite direction of the error increase
         w1 = w1 - n*step_w1;
         w2 = w2 - n*step_w2;
         b = b - n*step_b;
     }
 
+    // Output of the final converged parameters.
     std::cout << "===MLR Gradient Descent Result===\n";
     std::cout << "w1 = " << w1 << "\n";
     std::cout << "w2 = " << w2 << "\n";
