8d3db7fc5623cd6ecea322fdf009a1f57b956982
Alicja Potoniec <alicjahasinternet@outlook.com>
2026-03-29 04:19:40 +0100

    Called the header for R^2 and calculated the accuracy to output as %

--- Assignment_1/Task_2/SLR_solver_comparison.cpp
+++ Assignment_1/Task_2/SLR_solver_comparison.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip> 
+#include <vector>  
 #include "gradient_combined.h"
 #include "numerical_method_slr.h"
+#include "metrics.h"
 
 int main(){
-    // 1 == Equation Solver
+    // 1 == Equation Solver (Analytical)
     SLR_equation normal;
     normal.dataset();
     normal.solve();
 
-    // 2 == Gradient Solver
+    // 2 == Gradient Solver (Iterative)
     SLR_gradient gradient;
     gradient.dataset();
     gradient.solve();
@@ -19,14 +22,22 @@ int main(){
     double b_error = std::abs(normal.b_star - gradient.b);
 
     // 4 == Output Error Calcs
-    std::cout << "\n ===Equation vs the Gradient method===" << std::endl;
+    std::cout << "\n=== Equation vs the Gradient method ===" << std::endl;
     std::cout << "w difference = " << w_error << std::endl;
     std::cout << "b difference = " << b_error << std::endl;   
 
-    // 5 == Data Quality Analysis
-    double r = pearson_correlation(gradient.x, gradient.y);
-    std::cout << "\n ===Data Quality===" << std::endl;
-    std::cout << "Pearson correlation (r) = " << r << std::endl;
-    std::cout << "R-squared (r^2)) = " << (r * r) << std::endl;
+    // 5 == Data Quality & Model Fitness
+    // Generate the passive transformation prediction vector using the learned weights
+    std::vector<double> predictions = gradient.predict();
+    
+    // Pass the actual true y value and model's predictions to find accuracy
+    double Rsquared = calculate_Rsquared(gradient.y, predictions);
+
+    std::cout << "\n=== Data Quality ===" << std::endl;
+    std::cout << std::fixed << std::setprecision(4);
+    
+    // Multiply R^2 value by 100 to output %
+    std::cout << "Model Predictability: " << (Rsquared * 100.0) << "%" << std::endl;
+    
     return 0;
 }
